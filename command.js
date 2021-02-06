525b534564683591a411694448978d462a56b39e
RobertZSun <robertsunzhe@hotmail.com>
2021-02-07 01:29:57 +0800

    finish bath the version for smartphone and for the PC to draw the line

--- command.js
+++ command.js
@@ -3,48 +3,49 @@ canvas.width = document.documentElement.clientWidth;
 canvas.height = document.documentElement.clientHeight;
 var ctx = canvas.getContext("2d");
 
+// initialize the default line style
 ctx.fillStyle = "rgb(200,0,0)";
-// ctx.strokeStyle = 'none';
+ctx.lineWidth = 2;
+ctx.lineCap = "round";
 
+// is this a touch device?
+let isTouchDevice = "ontouchstart" in document.documentElement;
+
+// is the time to draw the line?
 let drawDots = false;
 
-canvas.onmousedown = () => {
-  drawDots = true;
-};
-
-canvas.onmouseup = () => {
-  drawDots = false;
-};
-
-canvas.onmousemove = (e) => {
-  if (drawDots) {
-    var x = e.clientX;
-    var y = e.clientY;
+// to see in which device so that to use different method
+// if the user draw the line on their smartphone
+if (isTouchDevice) {
+  canvas.ontouchstart = (e) => {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, 2 * Math.PI);
-    // ctx.stroke();
-    ctx.fill();
-  }
-};
+    ctx.moveTo(e.clientX, e.clientY);
+  };
 
-// ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-// ctx.fillRect(30, 30, 55, 50);
+  canvas.ontouchmove = (e) => {
+    var x = e.touches[0].clientX;
+    var y = e.touches[0].clientY;
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  };
+// if the user draw the line on PC
+} else {
+  canvas.onmousedown = (e) => {
+    drawDots = true;
+    ctx.beginPath();
+    ctx.moveTo(e.clientX, e.clientY);
+  };
 
-// var canvas_div = document.getElementById('canvas');
-// // canvas_div.onclick = (e) => {
-// canvas_div.onmousemove = (e) => {
-//     var x = e.clientX;
-//     var y = e.clientY;
-//     let div_to_create = document.createElement('div');
-//     div_to_create.style.position = 'absolute';
-//     div_to_create.style.left = x + 'px';
-//     div_to_create.style.top = y + 'px';
-//     div_to_create.style.backgroundColor = 'blue';
-//     // div_to_create.style.border = '1px solid green';
-//     div_to_create.style.width = '5px';
-//     div_to_create.style.height = '5px';
-//     div_to_create.style.marginLeft = '-3px';
-//     div_to_create.style.marginTop = '-3px';
-//     div_to_create.style.borderRadius = '50%';
-//     canvas_div.appendChild(div_to_create);
-// }
+  canvas.onmouseup = () => {
+    drawDots = false;
+  };
+
+  canvas.onmousemove = (e) => {
+    if (drawDots) {
+      var x = e.clientX;
+      var y = e.clientY;
+      ctx.lineTo(x, y);
+      ctx.stroke();
+    }
+  };
+}
